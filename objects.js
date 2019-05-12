993a449a50d32707147c65db4b33efe91fbb9a6f
Doug Loomer <loomerds@gmail.com>
2019-05-12 15:47:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {"Phil Ochs": "My Bloody Valentine"};
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {[artistName]: songTitle});
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
